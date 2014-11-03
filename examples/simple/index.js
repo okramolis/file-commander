823217483b9d111e12d0e196b337d04282fa3471
Ondrej Kramolis <o.kramolis@seznam.cz>
2014-11-03 20:05:09 +0100

    v0.1.0 Refactoring - more lightweight file-commander.js
    - plain app and main app does not render response for GET request method
      (they let the application to do it, but still provides the core functionality)
      => user-agent middleware moved to the application code
      => views middleware and rendering methods moved to the application code
    
    - file-commander exposes two application creators
      - serverBased => plain app
          => meant to be used for clients without JavaScript support
      - restBased => RESTful main app
          => meant to be used for REST capable clients with JavaScript support
    
    - REST api
      - no absolute links or hrefs used but data relative to current location sent
        => client has to put the data together to get the resulting address

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,8 +1,34 @@
-const STATIC_PATH         = '/static'
-  ,   COMMANDER_URL_MOUNT = '/commander'
-  ,   DEFAULT_PORT        = 8888
-  ,   STATIC_SETTINGS     = {
+const STATIC_PATH               = '/static'
+  ,   COMMANDER_URL_MOUNT       = '/commander'
+  ,   COMMANDER_ROUTE_EXP       = new RegExp('^' + COMMANDER_URL_MOUNT)
+  ,   DEFAULT_PORT              = 8888
+  ,   STATIC_SETTINGS           = {
         dotfiles: 'allow'
+      }
+  ,   VIEWS_PATH                = '/views'
+  ,   HTML5_VIEWS_MOUNT         = 'html5/'
+  ,   HTML4_VIEWS_MOUNT         = 'html4/'
+  ,   PLAIN_APP_URL_MOUNT       = COMMANDER_URL_MOUNT + '/plain-app'
+  ,   PLAIN_APP_ROUTE_EXP       = new RegExp('^' + PLAIN_APP_URL_MOUNT)
+  ,   PLAIN_APP_VIEWS_MOUNT     = 'apps/plain/'
+  ,   MAIN_APP_VIEWS_MOUNT      = 'apps/main/'
+      // TODO read html5 support from external file
+  ,   HTML5_SUPPORT             = {
+        "IE"                : {major: 11, minor: 0}
+      , "Firefox"           : {major: 24, minor: 0}
+      , "Chrome"            : {major: 24, minor: 0}
+      , "Safari"            : {major: 7 , minor: 0}
+      , "Opera"             : {major: 15, minor: 0}
+      , "Mobile Safari"     : {major: 7 , minor: 0}
+      , "Android"           : {major: 4 , minor: 4}
+      , "Opera Mobile"      : {major: 24, minor: 0}
+      , "BlackBerry WebKit" : {major: 10, minor: 0}
+      , "BlackBerry"        : {major: 10, minor: 0}
+      , "Chrome Mobile"     : {major: 38, minor: 0}
+      , "Chrome Mobile iOS" : {major: 38, minor: 0}
+      , "Firefox Mobile"    : {major: 32, minor: 0}
+      , "IE Mobile"         : {major: 10, minor: 0}
+      , "UC Browser"        : {major: 9 , minor: 9}
       }
 ;
 
@@ -14,6 +40,8 @@ const PORT = Number(process.argv[usrArgI++]) || DEFAULT_PORT;
 var express = require('express')
   , bodyParser = require('body-parser')
   , logger = require('morgan')
+  , path = require('path')
+  , useragent = require('useragent')
   , commander = require('../..')
 ;
 
@@ -23,6 +51,22 @@ var app = express();
 // -------------------------------------------------------------------
 // APP CONFIGURATION
 // -------------------------------------------------------------------
+
+// attributes configuration
+// -------------------------------------------------------------------
+app.locals = {
+  CONSTS: {
+    COMMANDER: {
+      URL_MOUNT           : COMMANDER_URL_MOUNT,
+      PLAIN_APP_URL_MOUNT : PLAIN_APP_URL_MOUNT
+    }
+  }
+};
+
+// render engine setup
+app.set('views', path.join(__dirname, VIEWS_PATH));
+app.set('view engine', 'jade');
+
 
 // middleware configuration
 // -------------------------------------------------------------------
@@ -35,12 +79,163 @@ app.use(logger('dev'));
 // serving public files
 app.use(express.static(__dirname + STATIC_PATH, STATIC_SETTINGS));
 
-// use file commander
-app.use(COMMANDER_URL_MOUNT, commander.app({
-  root    : __dirname,
-  mount   : COMMANDER_URL_MOUNT,
-  dotfiles: STATIC_SETTINGS.dotfiles
+// preprocess middleware
+app.use(reqInitMiddleware);
+
+// use file commander - PLAIN APP
+app.use(PLAIN_APP_URL_MOUNT, commander.serverBased({
+  publicFiles : __dirname + STATIC_PATH,
+  dotfiles    : STATIC_SETTINGS.dotfiles
 }));
+// process file commander result
+app.route(PLAIN_APP_ROUTE_EXP)
+.get(
+  validateCommanderMiddleware,
+  userAgentInitMiddleware,
+  resInitMiddleware,
+  viewsInitMiddleware,
+  getViewMiddleware(PLAIN_APP_VIEWS_MOUNT + 'index.jade'),
+  renderPlainAppMiddleware
+);
+// not found middleware sub-stack
+app.route(PLAIN_APP_ROUTE_EXP)
+.all(
+  notFoundCommonMiddleware,
+  notFoundRenderMiddleware
+);
+
+// use file commander - MAIN APP
+app.use(COMMANDER_URL_MOUNT, commander.restBased({
+  publicFiles : __dirname + STATIC_PATH,
+  dotfiles    : STATIC_SETTINGS.dotfiles,
+  mount       : COMMANDER_URL_MOUNT
+}));
+// process file commander result
+app.route(COMMANDER_ROUTE_EXP)
+.get(
+  validateCommanderMiddleware,
+  userAgentInitMiddleware,
+  resInitMiddleware,
+  viewsInitMiddleware,
+  getViewMiddleware(MAIN_APP_VIEWS_MOUNT + 'index.jade'),
+  renderMainAppMiddleware
+);
+// not found middleware sub-stack
+app.route(COMMANDER_ROUTE_EXP)
+.all(
+  notFoundCommonMiddleware,
+  notFoundRenderMiddleware
+);
+
+// -------------------------------------------------------------------
+// CUSTOM MIDDLEWARE DEFINITIONS
+// -------------------------------------------------------------------
+
+// renders main app response
+function renderMainAppMiddleware(req, res, next) {
+  res.locals.appkey.app.path = req.fcmder.fs.path.name;
+  simpleRenderer(req, res, next);
+}
+
+// renders plain app response
+function renderPlainAppMiddleware(req, res, next) {
+  res.locals.appkey.app.path  = req.fcmder.fs.path.name;
+  res.locals.appkey.app.files = req.fcmder.fs.files;
+  simpleRenderer(req, res, next);
+}
+
+
+// initializes request specific parameters
+// - shall be the first one from custom middleware chain
+function reqInitMiddleware(req, res, next) {
+  req.appkey = {
+    req: {
+      url: decodeURIComponent(req.url)
+    }
+  };
+  next();
+} // END of reqInitMiddleware
+
+// initializes response specific parameters
+function resInitMiddleware(req, res, next) {
+  res.locals = {
+    appkey: {
+      body: {},
+      app : {},
+      req : {
+        url   : req.appkey.req.url
+      }
+    }
+  };
+  next();
+} // END of resInitMiddleware
+
+// sets user-agent specific attributes regarding a current request
+function userAgentInitMiddleware(req, res, next) {
+  var ua = useragent.parse(req.headers['user-agent']);
+  console.log('userAgent:');
+  console.log(ua);
+
+  var html5min = HTML5_SUPPORT[ua.family];
+  req.appkey.ua = {
+    html4: (
+      !html5min ||
+      html5min.major >  ua.major ||
+      html5min.major == ua.major &&
+      html5min.minor >  ua.minor
+    )
+  };
+  next();
+} // END of userAgentInitMiddleware
+
+// check result of a processing of the commander app
+function validateCommanderMiddleware(req, res, next) {
+  if (!req.fcmder) {
+    next('route');
+    return;
+  }
+  next();
+} // END of validateCommanderMiddleware
+
+// sets path to render templates according to client's user-agent settings
+function viewsInitMiddleware(req, res, next) {
+  req.appkey.views = {
+    mount: (req.appkey.ua.html4) ? HTML4_VIEWS_MOUNT : HTML5_VIEWS_MOUNT
+  };
+  next();
+} // END of viewsInitMiddleware
+
+// sets view used in this middleware sub-stack
+function getViewMiddleware(view) {
+  return function(req, res, next) {
+    req.appkey.views.name = view;
+    next();
+  }
+} // END of getViewMiddleware
+
+// performs common actions regarding 404 status http status response
+function notFoundCommonMiddleware(req, res, next) {
+  res.status(404);
+  next();
+}
+
+// renders 404 page
+function notFoundRenderMiddleware(req, res, next) {
+  // TODO render 404 status page
+  res.send('Sorry, we cannot find that!!!!');
+}
+
+
+// -------------------------------------------------------------------
+// RESPONSE RENDERER DEFINITIONS
+// -------------------------------------------------------------------
+
+// renders response with given parameters without any further
+// modifications of the request or response
+function simpleRenderer(req, res, next, locals, cb) {
+  res.render(req.appkey.views.mount + req.appkey.views.name, locals, cb);
+}
+
 
 // -------------------------------------------------------------------
 // APP START
